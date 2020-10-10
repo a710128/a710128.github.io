3597bd2fdd8315f6e4be0bddfe7bdf4ad38c699c
zgy <qbjooo@qq.com>
2020-10-10 13:01:34 +0800

    add fzf 2

--- zsh.sh
+++ zsh.sh
@@ -1,6 +1,6 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" <&-
 #git clone https://github.com/powerline/fonts.git --depth=1 && ./fonts/install.sh && rm -rf fonts
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all  --completion --update-rc
 git clone https://github.com/fcamblor/oh-my-zsh-agnoster-fcamblor.git && ./oh-my-zsh-agnoster-fcamblor/install && rm -rf oh-my-zsh-agnoster-fcamblor
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting  ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
