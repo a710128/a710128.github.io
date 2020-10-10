aebdc590e68789e48e31efbdd771ecb63a22117b
zgy <qbjooo@qq.com>
2020-10-10 12:57:05 +0800

    add fzf

--- zsh.sh
+++ zsh.sh
@@ -1,7 +1,8 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" <&-
 #git clone https://github.com/powerline/fonts.git --depth=1 && ./fonts/install.sh && rm -rf fonts
-git clone https://github.com/fcamblor/oh-my-zsh-agnoster-fcamblor.git && ./oh-my-zsh-agnoster-fcamblor/install
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+git clone https://github.com/fcamblor/oh-my-zsh-agnoster-fcamblor.git && ./oh-my-zsh-agnoster-fcamblor/install && rm -rf oh-my-zsh-agnoster-fcamblor
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting  ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-mv ~/.zshrc ~/.zshrc.old && sed s/ZSH_THEME=.\*$/ZSH_THEME=\"agnoster\"/g ~/.zshrc.old | sed 's/^plugins=(/plugins=(zsh-autosuggestions zsh-syntax-highlighting /g'  > ~/.zshrc
+mv ~/.zshrc ~/.zshrc.old && sed s/ZSH_THEME=.\*$/ZSH_THEME=\"agnoster\"/g ~/.zshrc.old | sed 's/^plugins=(/plugins=(zsh-autosuggestions zsh-syntax-highlighting fzf /g'  > ~/.zshrc
 zsh
