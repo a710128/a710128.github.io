792de9809737eb3f7c5eaf2c01361d7ad6bf78f7
ZGY <qbjooo@qq.com>
2026-01-17 15:29:57 +0800

    Update opencode.sh

--- opencode.sh
+++ opencode.sh
@@ -2,3 +2,15 @@
 
 npm i -g opencode-ai@latest --registry=https://registry.npmmirror.com
 
+cat <<EOF > $HOME/.config/opencode/opencode.json
+{
+  "$schema": "https://opencode.ai/config.json",
+  "provider": {
+    "anthropic": {
+      "options": {
+        "baseURL": "https://api.ccodezh.com/v1"
+      }
+    }
+  }
+}
+EOF
